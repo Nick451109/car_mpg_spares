0eeb5698d3c60a9d1721e478e64d552112a261e4
Nick451109 <narevalo@espol.edu.ec>
2023-08-14 13:43:45 -0500

    guide 22

--- rest_spares/routes/rest_user.js
+++ rest_spares/routes/rest_user.js
@@ -1,22 +1,86 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { Carro, Persona } = require('../models'); // Asegúrate de importar tus modelos adecuadamente
 
-/* IMPORTE El ARCHIVO CON EL MODELO */
-const claseUser = require('../models').carro;
+// CREATE - Agregar un nuevo carro
+router.post('/carros', (req, res) => {
+  const { placa, modelo, marca, year, km, persona_idpersona } = req.body;
 
+  Carro.create({ placa, modelo, marca, year, km, persona_idpersona })
+    .then(carro => {
+      res.status(201).json(carro);
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).json({ error: 'Error al crear el carro' });
+    });
+});
 
-router.get('/findAll/json', function (req, res, next) {
+// READ - Obtener todos los carros
+router.get('/carros', (req, res) => {
+  Carro.findAll({ include: [Persona] })
+    .then(carros => {
+      res.status(200).json(carros);
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).json({ error: 'Error al obtener los carros' });
+    });
+});
 
-  /* MÉTODO ESTÁTICO findAll  */
+// READ - Obtener un carro por ID
+router.get('/carros/:id', (req, res) => {
+  const carroId = req.params.id;
 
-  claseUser.findAll({
-    attributes: { exclude: ["placa"] },
-  })
-  .then(resultado => {
-      res.json(resultado);
-  })
-  .catch(error => res.status(400).send(error))
+  Carro.findByPk(carroId, { include: [Persona] })
+    .then(carro => {
+      if (!carro) {
+        res.status(404).json({ error: 'Carro no encontrado' });
+      } else {
+        res.status(200).json(carro);
+      }
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).json({ error: 'Error al obtener el carro' });
+    });
+});
 
+// UPDATE - Actualizar un carro por ID
+router.put('/carros/:id', (req, res) => {
+  const carroId = req.params.id;
+  const { placa, modelo, marca, year, km, persona_idpersona } = req.body;
+
+  Carro.update({ placa, modelo, marca, year, km, persona_idpersona }, { where: { id: carroId } })
+    .then(([rowsUpdated]) => {
+      if (rowsUpdated === 0) {
+        res.status(404).json({ error: 'Carro no encontrado' });
+      } else {
+        res.status(200).json({ message: 'Carro actualizado exitosamente' });
+      }
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).json({ error: 'Error al actualizar el carro' });
+    });
+});
+
+// DELETE - Eliminar un carro por ID
+router.delete('/carros/:id', (req, res) => {
+  const carroId = req.params.id;
+
+  Carro.destroy({ where: { id: carroId } })
+    .then(rowsDeleted => {
+      if (rowsDeleted === 0) {
+        res.status(404).json({ error: 'Carro no encontrado' });
+      } else {
+        res.status(204).json({ message: 'Carro eliminado exitosamente' });
+      }
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).json({ error: 'Error al eliminar el carro' });
+    });
 });
 
 module.exports = router;
