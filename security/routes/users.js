c4421fde1c143572852a100c88e0eab83c0f23c1
Nick451109 <narevalo@espol.edu.ec>
2023-08-22 21:11:33 -0500

    fixy fix

--- security/routes/users.js
+++ security/routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* ACCEDA AL MÓDULO jwt */ 
 const jwt = require('jsonwebtoken');
 
@@ -13,31 +14,31 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-/* MANEJADOR DE LA RUTA '/login' */
-router.post('/login', function (req, res, next) {
+  /* MANEJADOR DE LA RUTA '/login' */
+  router.post('/login', function (req, res, next) {
 
-  const { nombre, contrasena } = req.body;
+    const { nombre, contrasena } = req.body;
 
-  Usuario.findOne({
-      where: {
-        [Op.and]: [
-          { nombre: nombre },
-          { contrasena: contrasena }
-        ]
-      }
-    })
-    .then(resultado => {
+    Usuario.findOne({
+        where: {
+          [Op.and]: [
+            { nombre: nombre },
+            { contrasena: contrasena }
+          ]
+        }
+      })
+      .then(resultado => {
 
-      /* GENERE EL accessToken CON LOS DATOS CIFRADOS */
-      const accessToken = jwt.sign({ nombre: resultado.nombre, rol: resultado.rol }, process.env.TOKEN_SECRET);
+        /* GENERE EL accessToken CON LOS DATOS CIFRADOS */
+        const accessToken = jwt.sign({ nombre: resultado.nombre, rol: resultado.rol }, process.env.TOKEN_SECRET);
 
-      res.json({
-        accessToken
-      });
+        res.json({
+          accessToken
+        });
 
-    })
-    .catch(error => res.status(400).send(error))
+      })
+      .catch(error => res.status(400).send(error))
 
-});
+  });
 
-module.exports = router;
+  module.exports = router;
